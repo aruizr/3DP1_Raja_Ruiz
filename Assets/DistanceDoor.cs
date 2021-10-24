9f3e70fc65a09ba788fd5f40dcf45d635e328aa9
GuillemRX <63998127+GuillemRX@users.noreply.github.com>
2021-10-24 11:05:35 +0200

    Regular Door

--- Assets/DistanceDoor.cs
+++ Assets/DistanceDoor.cs
@@ -4,31 +4,21 @@ using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 
-public class DistanceDoor : MonoBehaviour
+public class DistanceDoor : Door
 {
-    [SerializeField] private Transform doorTransform;
 
-    private float _originalPositionY, _finalPositionY;
-
-    [SerializeField]
-    private float _offsetY;
-
-    private void Awake()
-    {
-        _originalPositionY = doorTransform.position.y;
-        _finalPositionY = _originalPositionY - _offsetY;
-    }
 
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.CompareTag("Player"))
         {
-            doorTransform.DOMoveY(_finalPositionY, 0.5f);
+            OpenDoor();
         }
     }
 
     private void OnTriggerExit(Collider other){
         if(other.gameObject.CompareTag("Player")){
-            doorTransform.DOMoveY(_originalPositionY, 0.5f);
+            CloseDoor();
+            
         }
     }
 
